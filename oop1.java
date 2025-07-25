e1c87750fb7c8c1a647a7f12eb381d55c57a8748
Kamal P <kamaldhanam888@gmail.com>
2025-07-25 14:50:48 +0530

    Add files via upload

---
+++ oop1.java
@@ -0,0 +1,27 @@
+package ooppractice;
+class Car{
+    // private instance variable
+    private String carname;
+    private String modelname;
+    private int year;
+    //constructors to intialize value
+    public Car(String carname,String modelname,int year) {
+        this.carname=carname;
+        this.modelname=modelname;
+        this.year=year;
+    }
+    // method to print the car detail
+    public void display(){
+        System.out.println("CarName : "+carname);
+        System.out.println("modelname : "+modelname);
+        System.out.println("Year : "+ year);
+    }
+}
+public class oop1{
+    public static void main(String[] args) {
+        Car obj=new Car("Toyato","T2",2015);
+        obj.display();
+
+
+    }
+}
